9c1567076d39e904e3d395c788ae7ed726cd7342
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2021-12-24 20:11:36 +0530

    feat: new colors & shadows

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ const signUpBtn = document.querySelector("#signUp");
 const signInBtn = document.querySelector("#signIn");
 const container = document.querySelector("#containerId");
 
-signUpBtn.addEventListener("click", () => {
+signUpBtn.addEventListener("click", e => {
+  e.preventDefault();
   container.classList.add("right_panel_active");
 });
 
-signInBtn.addEventListener("click", () => {
+signInBtn.addEventListener("click", e => {
+  e.preventDefault();
   container.classList.remove("right_panel_active");
 });
